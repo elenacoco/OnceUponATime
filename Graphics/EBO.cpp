36a3da0d64d46ad0fb7fde46958650bd04b614e2
giulitufanari <ggiulia.tufanari@gmail.com>
2025-05-28 15:49:54 +0200

    EBO

---
+++ Graphics/EBO.cpp
@@ -0,0 +1,31 @@
+#include "EBO.h"
+
+EBO::EBO(const std::vector<GLuint>& indices)
+{
+    glGenBuffers(1, &id);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), indices.data(), GL_STATIC_DRAW);
+}
+
+
+EBO::~EBO()
+{
+    glDeleteBuffers(1, &id);
+}
+
+void EBO::bind() const
+{
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id); //collega il buffer al target
+}
+
+void EBO::unbind() const
+{
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+GLuint EBO::getID() const
+{
+    return id;
+}
+
+
--- Graphics/EBO.h
+++ Graphics/EBO.h
@@ -1,3 +1,22 @@
 #pragma once
 //ELEMENT BUFFER OBJECT
 
+#include <vector>
+#include <glad/glad.h>
+
+class EBO
+{
+private:
+    GLuint id;
+
+public:
+    EBO(const std::vector<GLuint>& indices);
+    ~EBO();
+
+    void bind() const;
+    void unbind() const;
+
+    GLuint getID() const;
+};
+
+
