16a377fdcf92a44a510f5c837c8301e9b481f5d9
elenacoco <elenacoco2003@gmail.com>
2025-05-28 12:36:01 +0200

    struct vertex

--- Graphics/Vertex.h
+++ Graphics/Vertex.h
@@ -1,7 +1,14 @@
-//#pragma once
-//#include<Libre>
-//struct Vecrtex
-//{
-//	Vector3f position;
-//
-//};
+#pragma once
+#include <AlgebraLineare.h>
+
+
+struct Vertex
+{
+	Vector3f position;
+	Vector3f color;
+	Vector2f texCoords;
+	Vector3f normal;
+	Vector3f tangent;
+	Vector3f bitangent;
+
+};
