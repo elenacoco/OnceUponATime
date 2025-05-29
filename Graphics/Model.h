a841c0f771f2c0afd74f3d3807f8ac6676999423
elenacoco <elenacoco2003@gmail.com>
2025-05-29 16:25:15 +0200

    model

--- Graphics/Model.h
+++ Graphics/Model.h
@@ -1,18 +1,29 @@
 #pragma once
-#include<string>;
+#include <string>
+#include "Mesh.h"
+#include "Shader.h"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
  
 using namespace std;
+using namespace Assimp;
 
+class Model
+{
+public:
 
-class Model {
+    vector<Mesh> meshes;
+    string directory;
 
-public : 
+    Model(char* path);
 
-	string name;
-	string path;
+    void drawModel(Shader& shader);
 
+private:
 
-
-
-	Model(string name, string path);
+    void loadModel(string path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, string typeName);
 };
